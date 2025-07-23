d4d46b84bb49b2d46f73b3d13f47c6cb0a0dfdc2
michaelchinkov <michael.chinkov@icloud.com>
2025-07-23 12:49:38 -0700

    Fixed class errors, fun fact: true and false are lower case.

--- Java Projects/APCSA_Prep/JavaRecursionLab/src/FancyCalc.java
+++ Java Projects/APCSA_Prep/JavaRecursionLab/src/FancyCalc.java
@@ -1,74 +1,68 @@
 public class FancyCalc {
-    public static int fib(int n){
+
+
+    public static int fib(int n) {
         return helpFib(n);
     }
-    private static int helpFib(int n){
-        if (n == 1||n == 2)
-        {
+
+    private static int helpFib(int n) {
+        if (n == 1 || n == 2) {
             return 1;
-        }
-        else {
-            return helpFib(n-1)+helpFib(n-2);
+        } else {
+            return helpFib(n - 1) + helpFib(n - 2);
         }
     }
+
     public static void main(String[] args) {
         System.out.println(fib(5));
     }
-    /*
-}
-    public int fact(n)
-    {
+
+    public int fact(int n) {
         return this.factHelp(n, 1);
     }
-    {
-    private int factHelp(n, result){
-            if (n == 0)
-                return result;
-            else
-                return this.factHelp(n – 1, n*result);
-    }
 
-}
-    public isPrime(int num)
-    {
-        return this.helpPrime(num, 2);
-    }
-    {
-    private helpPrime(int num,int divisor){
-        if (divisor == num)
-            return True;
-        if (num % divisor == 0)
-            return False;
-        return this.helpPrime(num, divisor+1);
-    }
-    public static int pow(int base, int exponent) {
-        if (exponent == 0)
-        {
-            return 1;
+    private int factHelp(int n, int result) {
+        if (n == 0) {
+            return result;
         }
         else {
+            return this.factHelp((n - 1), n * result);
+        }
+    }
+
+    public boolean isPrime(int num) {
+        return this.helpPrime(num, 2);
+    }
+
+    private boolean helpPrime(int num, int divisor) {
+        if (divisor == num)
+            return true;
+        if (num % divisor == 0)
+            return false;
+        return this.helpPrime(num, divisor + 1);
+    }
+
+    public int pow(int base, int exponent) {
+        if (exponent == 0) {
+            return 1;
+        } else
+        {
             return pow(base, exponent - 1) * base;
         }
+    }
 
-
-    public int fastPow(int base, int exponent) {
-            //base case
-            if (exponent == 0) return 1;
-            if (exponent % 2 == 0)
-            {
-                return this.square(this.fastPow(base, exponent / 2));
-            }
-            return base * this.fastPow(base, exponent-1);
+    public int fastPow ( int base, int exponent){
+        //base case
+        if (exponent == 0) return 1;
+        if (exponent % 2 == 0) {
+            return this.square(this.fastPow(base, exponent / 2));
         }
-        public int square(int x){
-        return x*x;
+        return base * this.fastPow(base, exponent - 1);
     }
-    public static void main(String[] args) {
-        FancyCalc fp = new FancyCalc();
-        System.out.println(fp.fastPow(4, 5));
+    public int square ( int x){
+        return x * x;
     }
-    //Struggled with this pretty hard because I forgot the difference from static and instantiatic methods, I had to consulte with
-    //chatGPT to wrap my head around why my test class was throwing errors
+        //Struggled with this pretty hard because I forgot the difference from static and instantiatic methods, I had to consulte with
+        //chatGPT to wrap my head around why my test class was throwing errors
 
-     */
 }
