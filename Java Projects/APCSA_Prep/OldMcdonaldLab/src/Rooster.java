ca2784cbed74caffacc6d6b1ad8539743fa11106
michaelchinkov <michael.chinkov@icloud.com>
2025-07-29 13:26:05 -0700

    Created Rooster.java

---
+++ Java Projects/APCSA_Prep/OldMcdonaldLab/src/Rooster.java
@@ -0,0 +1,9 @@
+public class Rooster extends Chicken{
+    public Rooster(){
+        super("Rooster");
+    }
+
+    public void speak(){
+        System.out.println("cock-a-doodle-doo");
+    }
+}
