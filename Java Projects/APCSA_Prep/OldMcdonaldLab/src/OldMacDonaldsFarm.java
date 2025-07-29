f82f0d6c6bbb3cd352f69a2456d3d87883c64d62
michaelchinkov <michael.chinkov@icloud.com>
2025-07-29 13:42:07 -0700

    Created tester class OldMacDonaldsFarm.java

--- Java Projects/APCSA_Prep/OldMcdonaldLab/src/OldMacDonaldsFarm.java
+++ Java Projects/APCSA_Prep/OldMcdonaldLab/src/OldMacDonaldsFarm.java
@@ -3,9 +3,26 @@ import java.util.ArrayList;
 public class OldMacDonaldsFarm {
     private String farmerName;
     private ArrayList<Animal> farmAnimals;
-    public OldMacDonaldsFarm()
-    {
+
+    public OldMacDonaldsFarm() {
         farmerName = "Old MacDonald";
-        farmAnimals = new ArrayList<Animal>( );
+        farmAnimals = new ArrayList<Animal>();
     }
+
+    public static void main(String[] args) {
+
+
+    OldMacDonaldsFarm singer = new OldMacDonaldsFarm();
+        singer.farmAnimals.add(new Chicken());
+        singer.singVerse();
+        singer.farmAnimals.add(new Chick());
+        singer.singVerse();
+        singer.farmAnimals.add(new ooster());
+        singer.singVerse();
+        singer.farmAnimals.add(new Pig());
+        singer.singVerse();
 }
+}
+
+
+
