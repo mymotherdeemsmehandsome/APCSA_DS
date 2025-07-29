1e48af232fc3e6f2d603160843e0cd5be817ed95
michaelchinkov <michael.chinkov@icloud.com>
2025-07-29 13:37:32 -0700

    Created tester class OldMacDonaldsFarm.java

---
+++ Java Projects/APCSA_Prep/OldMcdonaldLab/src/OldMacDonaldsFarm.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class OldMacDonaldsFarm {
+    private String farmerName;
+    private ArrayList<Animal> farmAnimals;
+    public OldMacDonaldsFarm()
+    {
+        farmerName = "Old MacDonald";
+        farmAnimals = new ArrayList<Animal>( );
+    }
+}
