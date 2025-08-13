bb43a0d50d455f504534156660094de3bea88b6f
Michael Chinkov <140359281+mymotherdeemsmehandsome@users.noreply.github.com>
2025-08-13 13:41:47 -0700

    Delete Python Projects directory
    Woops!

--- Python Projects/YobeeProjects/AreaCodeFiltering/300Miles.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('/Users/michaelchinkov/Documents/Programing Projects/Python Projects/YobeeProjects/AreaCodeFiltering/Half Home Users Export Jan 27 2025.csv')
-
-phone = df[(df["Phone"])]
-
